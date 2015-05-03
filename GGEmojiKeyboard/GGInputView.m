b20f392ab492b716af5240ef0f908d6f1ad30f3d
zero <osmk@qq.com>
2015-05-03 18:03:24 +0800

    build

---
+++ GGEmojiKeyboard/GGInputView.m
@@ -0,0 +1,21 @@
+//
+//  GGInputView.m
+//  GGEmojiKeyboard
+//
+//  Created by __无邪_ on 15/5/3.
+//  Copyright (c) 2015年 __无邪_. All rights reserved.
+//
+
+#import "GGInputView.h"
+
+@implementation GGInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
